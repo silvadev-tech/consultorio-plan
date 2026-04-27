eeab10a1e7d9e5d71c54338dce61e86436dedefe
Rodrigo <rodrigolucasdasilva05@gmail.com>
2026-04-27 10:59:25 -0300

    ajustes main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import axios from 'axios'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+axios.defaults.baseURL = 'https://railway.app';
+ReactDOM.createRoot(document.getElementById('root')).render(
+<ReactDOM.StrictMode>
+<App />
+</React.StrictMode>,
 )
+
